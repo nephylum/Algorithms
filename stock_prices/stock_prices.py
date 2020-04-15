91a5c451a857a9146bdc00db7d3a32e1dc8cb06e
nephylum <25045705+nephylum@users.noreply.github.com>
2020-04-15 14:36:20 -0700

    updated stock_prices.py

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -10,14 +10,14 @@ def find_max_profit(prices):
             if prices[y] - prices[x] > max_profit:
                 max_profit = prices[y] - prices[x]
     return max_profit
-
+print("find_max_profit([1050, 270, 1540, 3800, 2])  returns:")
 print(find_max_profit([1050, 270, 1540, 3800, 2]))
 
 
-# if __name__ == '__main__':
-#   # This is just some code to accept inputs from the command line
-#   parser = argparse.ArgumentParser(description='Find max profit from prices.')
-#   parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-#   args = parser.parse_args()
-#
-#   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+if __name__ == '__main__':
+  # This is just some code to accept inputs from the command line
+  parser = argparse.ArgumentParser(description='Find max profit from prices.')
+  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+  args = parser.parse_args()
+
+  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
