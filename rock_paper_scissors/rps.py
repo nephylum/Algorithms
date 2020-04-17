57f61409f2cef4fcd24bc2a1bcfda8d6c6a32886
nephylum <25045705+nephylum@users.noreply.github.com>
2020-04-16 17:02:26 -0700

    finished

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,30 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+    if n == 0:
+        return [[]]
+    poss_index = [0] * n
+    rps = ['rock','paper','scissors']
+    lol = [[rps[x] for x in poss_index]]
+
+    done = False
+    while done == False:
+        poss_index[n-1] += 1
+
+        for x in range(1, n+1):
+            if poss_index[n-x] == 3:
+                poss_index[n-x] = 0
+                if n-x+1 >= 0:
+                    poss_index[n-(x+1)] += 1
+        list = [rps[x] for x in poss_index]
+        lol.append(list)
+        if all(x == 2 for x in poss_index):
+            done = True
+
+
+
+
+    return lol
 
 
 if __name__ == "__main__":
